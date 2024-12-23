ffb72fa61da7280719331af95e163dec4d701d8e
zhuganglie <pyrrhonianpig@gmail.com>
2024-12-23 22:56:13 +0800

    update

--- post_template.py
+++ post_template.py
@@ -1,12 +1,1 @@
-from fasthtml.common import *
 
-def post_detail_template(title, date, tags, content):
-    return Div(
-        Div(
-            H1(title, **{"class": "text-3xl font-bold text-center"}),
-            P(f"Date: {date}", **{"class": "text-gray-600 text-center"}),
-            **{"class": "text-center"}
-        ),
-        Div(f"<zero-md>{content}</zero-md>", **{"class": "my-4"}),
-        Div("Tags: ", *[A(tag, href=f"/tag/{tag}", cls="text-blue-500 hover:text-blue-700 no-underline mr-2") for tag in tags], cls= "text-gray-500 text-left")
-    )
