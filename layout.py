87d62230f3911f8b139862a66102c3f9c69cc9f5
zhuganglie <pyrrhonianpig@gmail.com>
2025-01-15 22:43:44 +0800

    update

--- layout.py
+++
@@ -1,5 +0,0 @@
-from fasthtml.components import Container, Titled, Main
-from nav import navbar
-
-def layout(title, *content):
-    return Titled(title, Main(navbar(), Container(*content, cls="p-4")))
--- nav.py
+++
@@ -1,8 +0,0 @@
-from fasthtml.components import A, Li, Ul, Div
-
-def navbar():
-    links = [
-        Li(A("Home", href="/")),
-        Li(A("Tags", href="/tags")),
-    ]
-    return Div(Ul(*links), cls="bg-gray-800 p-4")
